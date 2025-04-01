4bf11b3fb9e64e6230fb230a7bd6b68156eb49c7
CORACAKUN <ramoncarlconda726@gmail.com>
2025-04-01 23:36:12 +0800

    added slider at galler and combine docEv into one (reuse)

--- script.js
+++ script.js
@@ -2,26 +2,54 @@ document.addEventListener("DOMContentLoaded", function () {
   const images = ["images/bg.png", "images/bg1.png", "images/bg2.png"];
 
   let currentIndex = 0;
+  
+  // Background Slider
   const container = document.querySelector(".container1");
-  const leftBtn = document.querySelector(".left-btn");
-  const rightBtn = document.querySelector(".right-btn");
+  const leftBtnBg = document.querySelector(".left-btn-bg");
+  const rightBtnBg = document.querySelector(".right-btn-bg");
 
   function changeBackground(index) {
     container.style.backgroundImage = `url(${images[index]})`;
   }
 
-  function nextImage() {
+  function nextBackground() {
     currentIndex = (currentIndex + 1) % images.length;
     changeBackground(currentIndex);
+  }
+
+  function prevBackground() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    changeBackground(currentIndex);
+  }
+
+  if (rightBtnBg && leftBtnBg) {
+    rightBtnBg.addEventListener("click", nextBackground);
+    leftBtnBg.addEventListener("click", prevBackground);
+  }
+
+  setInterval(nextBackground, 5000);
+
+  // Gallery Image Slider
+  const galleryImage = document.getElementById("galleryImage");
+  const leftBtn = document.getElementById("left-btn");
+  const rightBtn = document.getElementById("right-btn");
+
+  function changeImage(index) {
+    galleryImage.src = images[index];
+  }
+
+  function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    changeImage(currentIndex);
   }
 
   function prevImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    changeBackground(currentIndex);
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    changeImage(currentIndex);
   }
 
-  rightBtn.addEventListener("click", nextImage);
-  leftBtn.addEventListener("click", prevImage);
-
-  setInterval(nextImage, 5000);
+  if (rightBtn && leftBtn) {
+    rightBtn.addEventListener("click", nextImage);
+    leftBtn.addEventListener("click", prevImage);
+  }
 });
